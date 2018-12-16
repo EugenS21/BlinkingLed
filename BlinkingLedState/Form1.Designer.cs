9baf69e3de58486f5261806d8e1917573d123f2f
EugenS21 <Rzl7kxJJ^%3QL^D70zC2>
2018-12-16 23:42:14 +0200

    add folders and files

---
+++ BlinkingLedState/Form1.Designer.cs
@@ -0,0 +1,69 @@
+﻿namespace BlinkingLedState
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.myPictureBox = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.myPictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // myPictureBox
+            // 
+            this.myPictureBox.InitialImage = null;
+            this.myPictureBox.Location = new System.Drawing.Point(0, -1);
+            this.myPictureBox.Margin = new System.Windows.Forms.Padding(0);
+            this.myPictureBox.Name = "myPictureBox";
+            this.myPictureBox.Size = new System.Drawing.Size(237, 212);
+            this.myPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.myPictureBox.TabIndex = 0;
+            this.myPictureBox.TabStop = false;
+            this.myPictureBox.MouseClick += new System.Windows.Forms.MouseEventHandler(this.myPictureBox_MouseClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(239, 213);
+            this.Controls.Add(this.myPictureBox);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.Text = "Led State Switching App";
+            this.TopMost = true;
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.myPictureBox)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox myPictureBox;
+    }
+}
+
---
+++ BlinkingLedState/Form1.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+
+namespace BlinkingLedState
+{ 
+    public partial class Form1 : Form
+    { 
+        private string responseContent;
+        enum imageColor {GREEN, RED};
+        int activeImage; 
+
+        public Form1()
+        {
+            InitializeComponent(); 
+        }
+
+        public Image getFirstImage(string url)
+        { 
+            return getImage(url);
+        }
+
+        public Image getSecondImage(string url)
+        { 
+            return getImage(url);
+        }
+
+        public System.Drawing.Image getImage(string imageUrl)
+        {
+            System.Drawing.Image image = null;
+            try
+            {
+                System.Net.HttpWebRequest webRequest = (System.Net.HttpWebRequest)System.Net.HttpWebRequest.Create(imageUrl);
+                webRequest.AllowWriteStreamBuffering = true;
+                webRequest.Timeout = 30000;
+                System.Net.WebResponse webResponse = webRequest.GetResponse();
+                System.IO.Stream stream = webResponse.GetResponseStream();
+                image = System.Drawing.Image.FromStream(stream);
+                webResponse.Close();
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+            return image;
+        } 
+
+        static async Task GetRequest(string url, Action<string> resultMessage)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                using (HttpResponseMessage response = await client.GetAsync(url))
+                {
+                    using (HttpContent content = response.Content)
+                    {
+                        string myResponse = await content.ReadAsStringAsync(); 
+                        resultMessage?.Invoke(myResponse);
+                    }
+                }
+            }
+
+        } 
+
+        public void getResponse (string input)
+        {
+            responseContent = input;
+        }
+
+        public void Form1_Load(object sender, EventArgs e)
+        {
+            Regex responseContentPattern = new Regex(@"(?<=.*state.*: )\d+(?=,)");
+            Match responseMatch;
+            Action<string> myAction = getResponse;
+            GetRequest("http://35.190.129.6:3000/led_state", myAction)
+                .ContinueWith (task =>
+                {
+                    responseMatch = responseContentPattern.Match(responseContent); 
+                    if (responseMatch.Value=="0") { 
+                        myPictureBox.Image = getFirstImage("http://www.clker.com/cliparts/5/7/b/5/1194989231691813435led_circle_red.svg.hi.png");
+                        activeImage = (int)imageColor.RED;
+                    }
+                    else {
+                        
+                        myPictureBox.Image = getSecondImage("http://www.clker.com/cliparts/1/5/4/b/11949892282132520602led_circle_green.svg.med.png");
+                        activeImage = (int)imageColor.GREEN;
+                    }  
+                });  
+        } 
+
+        public void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        { 
+            // your code here
+        }
+
+        private void myPictureBox_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (activeImage==0)
+            {
+                //Green ONE
+                myPictureBox.Image = getFirstImage("http://www.clker.com/cliparts/5/7/b/5/1194989231691813435led_circle_red.svg.hi.png");
+                activeImage = (int)imageColor.RED;
+            }
+            else
+            {
+                myPictureBox.Image = getSecondImage("http://www.clker.com/cliparts/1/5/4/b/11949892282132520602led_circle_green.svg.med.png");
+                activeImage = (int)imageColor.GREEN;
+            }
+        }
+    }
+}
